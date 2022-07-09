af6ea70f5a623aabee6a78f526aaefb260c205f4
Arun J <Arunj@nextgen.com>
2022-07-09 23:06:24 +0530

    First commit

---
+++ ArunFramework/ArunFramework.h
@@ -0,0 +1,18 @@
+//
+//  ArunFramework.h
+//  ArunFramework
+//
+//  Created by Arun J on 09/07/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ArunFramework.
+FOUNDATION_EXPORT double ArunFrameworkVersionNumber;
+
+//! Project version string for ArunFramework.
+FOUNDATION_EXPORT const unsigned char ArunFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ArunFramework/PublicHeader.h>
+
+
---
+++ ArunFramework/FirstClass.swift
@@ -0,0 +1,14 @@
+//
+//  FirstClass.swift
+//  ArunFramework
+//
+//  Created by Arun J on 09/07/22.
+//
+
+import Foundation
+
+public class FirstClass {
+    public func firstFunction() {
+        print("First class working")
+    }
+}
